ca890527f3a45226b2fc4b21ed081b830ce20842
cuamatzi09 <luisangelcuamatzi@gmail.com>
2025-05-25 11:56:00 -0600

    Se agrega al proyecto

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'includes/header.php';
         <p class="lead">Descubre el lujo y la comodidad en nuestro exclusivo hotel</p>
         <?php if (!isset($_SESSION['user_id'])): ?>
             <a href="register.php" class="btn btn-primary btn-lg me-2">Registrarse</a>
-            <a href="login.php" class="btn btn-outline-light btn-lg">Iniciar Sesión</a>
+            <a href="login.php" class="btn btn-primary btn-lg">Iniciar Sesión</a>
         <?php else: ?>
             <a href="availability.php" class="btn btn-primary btn-lg">Reservar Ahora</a>
         <?php endif; ?>
@@ -27,13 +27,13 @@ require_once 'includes/header.php';
                 echo '
                 <div class="col-md-4 mb-4">
                     <div class="card h-100">
-                        <img src="'.SITE_URL.'/assets/images/'.$room['image_url'].'" class="card-img-top" alt="'.$room['room_type'].'">
+                        <img src="' . SITE_URL . '/assets/images/' . $room['image_url'] . '" class="card-img-top" alt="' . $room['room_type'] . '">
                         <div class="card-body">
-                            <h5 class="card-title">'.$room['room_type'].'</h5>
-                            <p class="card-text">'.$room['description'].'</p>
+                            <h5 class="card-title">' . $room['room_type'] . '</h5>
+                            <p class="card-text">' . $room['description'] . '</p>
                             <ul class="list-group list-group-flush mb-3">
-                                <li class="list-group-item"><i class="fas fa-bed"></i> Capacidad: '.$room['capacity'].' personas</li>
-                                <li class="list-group-item"><i class="fas fa-dollar-sign"></i> Precio: $'.number_format($room['price_per_night'], 2).' por noche</li>
+                                <li class="list-group-item"><i class="fas fa-bed"></i> Capacidad: ' . $room['capacity'] . ' personas</li>
+                                <li class="list-group-item"><i class="fas fa-dollar-sign"></i> Precio: $' . $room['price_per_night'] . ' por noche</li>
                             </ul>
                         </div>
                         <div class="card-footer bg-transparent">
@@ -43,6 +43,26 @@ require_once 'includes/header.php';
                 </div>';
             }
             ?>
+        </div>
+    </div>
+</section>
+
+<section class="py-5 bg-light">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h2>Servicios Exclusivos</h2>
+                <ul class="list-unstyled">
+                    <li class="mb-3"><i class="fas fa-wifi text-primary me-2"></i> WiFi de alta velocidad gratuito</li>
+                    <li class="mb-3"><i class="fas fa-swimming-pool text-primary me-2"></i> Piscina climatizada</li>
+                    <li class="mb-3"><i class="fas fa-spa text-primary me-2"></i> Spa y centro de bienestar</li>
+                    <li class="mb-3"><i class="fas fa-utensils text-primary me-2"></i> Restaurante gourmet</li>
+                    <li class="mb-3"><i class="fas fa-concierge-bell text-primary me-2"></i> Servicio a la habitación 24/7</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <img src="<?php echo SITE_URL; ?>/assets/images/hotel-services.jpg" alt="Servicios del hotel" class="img-fluid rounded">
+            </div>
         </div>
     </div>
 </section>
